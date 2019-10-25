4e4c4958ab1051a1df712c96f323c8fb0c204e87
LEESUNAE <sjsao5012@gmail.com>
2019-10-25 19:28:38 +0900

    library 사용

--- app/src/main/java/com/yeonae/chamelezone/PlaceDetailActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/PlaceDetailActivity.kt
@@ -1,5 +1,6 @@
 package com.yeonae.chamelezone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_place_detail.*
@@ -12,5 +13,9 @@ class PlaceDetailActivity: AppCompatActivity() {
 
         val imageAdapter = ImageViewPagerAdapter()
         view.adapter = imageAdapter
+
+        review.setOnClickListener {
+            val intent = Intent(this, ReviewCreateActivity::class.java)
+            startActivity(intent)   }
     }
 }
---
+++ app/src/main/java/com/yeonae/chamelezone/ReviewCreateActivity.kt
@@ -0,0 +1,13 @@
+package com.yeonae.chamelezone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReviewCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.review_create)
+
+    }
+
+}
