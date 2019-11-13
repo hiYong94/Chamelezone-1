c3843ea50badd4c0c2fe3459d472478ef1a2cc75
LEESUNAE <sjsao5012@gmail.com>
2019-11-14 01:16:39 +0900

    갤러리에서 이미지 불러오기

--- app/src/main/java/com/yeonae/chamelezone/MapsFragment.kt
+++ app/src/main/java/com/yeonae/chamelezone/MapsFragment.kt
@@ -22,167 +22,170 @@ import java.util.*
 
 class MapsFragment : Fragment(), OnMapReadyCallback,
     ActivityCompat.OnRequestPermissionsResultCallback{
-
-    private var map: GoogleMap? = null
-    private var currentMarker: Marker? = null
-    var currentLocation: Location? = null
-    lateinit var currentPosition: LatLng
-
-    private var fusedLocationClient: FusedLocationProviderClient? = null
-    private var locationRequest: LocationRequest? = null
-    private var location: Location? = null
-
-    private var locationCallback: LocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult?) {
-            super.onLocationResult(locationResult)
-            val locationList = locationResult?.locations
-
-            if (locationList?.size!! > 0) {
-                location = locationList[locationList.size - 1]
-
-                currentPosition = LatLng(location!!.latitude, location!!.longitude)
-
-                val markerTitle = getCurrentAddress(currentPosition)
-                val markerSnippet = ("위도:" + location!!.latitude.toString()
-                        + " 경도:" + location!!.longitude.toString())
-
-                setCurrentLocation(location, markerTitle, markerSnippet)
-                currentLocation = location
-            }
-        }
+    override fun onMapReady(p0: GoogleMap?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        map_view.run {
-            onCreate(savedInstanceState)
-            onResume()
-            getMapAsync(this@MapsFragment)
-        }
-
-        locationRequest = LocationRequest()
-            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY) //정확한 위치 요청
-            .setInterval(UPDATE_INTERVAL_MS.toLong()) //활성 위치 업데이트 간격
-            .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS.toLong()) //위치 업데이트 간격
-
-        LocationSettingsRequest.Builder().addLocationRequest(locationRequest!!)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(App.instance.context())
-
-    }
-
-    override fun onMapReady(googleMap: GoogleMap?) {
-        map = googleMap
-
-        setDefaultLocation()
-        startLocationUpdates()
-
-        map?.run {
-            isMyLocationEnabled = true
-            uiSettings.isMyLocationButtonEnabled = true
-        }
-
-        btn_search.setOnClickListener {
-            searchLocation()
-        }
-    }
-
-    private fun startLocationUpdates() {
-        fusedLocationClient?.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            Looper.myLooper()
-        )
-    }
-
-    fun getCurrentAddress(latLng: LatLng): String {
-        val geoCoder = Geocoder(App.instance.context(), Locale.getDefault())
-        val addresses = geoCoder.getFromLocation(
-            latLng.latitude,
-            latLng.longitude,
-            1
-        )
-        return addresses[0].getAddressLine(0).toString()
-    }
-
-    fun setCurrentLocation(location: Location?, markerTitle: String, markerSnippet: String) {
-        currentMarker?.remove()
-
-        val currentLatLng = LatLng(location!!.latitude, location.longitude)
-
-        val markerOptions = MarkerOptions().apply {
-            position(currentLatLng)
-            title(markerTitle)
-            snippet(markerSnippet)
-            draggable(true)
-        }
-
-        currentMarker = map?.addMarker(markerOptions)
-        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
-
-    }
-
-    private fun setDefaultLocation() {
-        val defaultLocation = LatLng(37.56, 126.97)
-        val markerTitle = "위치정보 가져올 수 없음"
-        val markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요"
-
-        currentMarker?.remove()
-
-        val markerOptions = MarkerOptions().apply {
-            position(defaultLocation)
-            title(markerTitle)
-            snippet(markerSnippet)
-            draggable(true)
-            icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-        }
-
-        currentMarker = map?.addMarker(markerOptions)
-        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 15f))
-
-    }
-
-    private fun searchLocation() {
-        map?.clear()
-        val geoCoder = Geocoder(App.instance.context(), Locale.getDefault())
-        val addresses = geoCoder.getFromLocationName(
-            edt_search.text.toString(),
-            10
-        )
-
-        if(addresses.size > 0){
-            val searchLatLng = LatLng(addresses[0].latitude, addresses[0].longitude)
-
-            val markerTitle = getCurrentAddress(searchLatLng)
-
-            val markerOptions = MarkerOptions().apply {
-                position(searchLatLng)
-                title(markerTitle)
-                draggable(true)
-            }
-
-            map?.run {
-                addMarker(markerOptions)
-                animateCamera(CameraUpdateFactory.newLatLngZoom(searchLatLng, 15f))
-            }
-        }
-
-
-
-
-
-    }
-
-    companion object {
-        private const val UPDATE_INTERVAL_MS = 1000  // 1초
-        private const val FASTEST_UPDATE_INTERVAL_MS = 500 // 0.5초
-    }
+//    private var map: GoogleMap? = null
+//    private var currentMarker: Marker? = null
+//    var currentLocation: Location? = null
+//    lateinit var currentPosition: LatLng
+//
+//    private var fusedLocationClient: FusedLocationProviderClient? = null
+//    private var locationRequest: LocationRequest? = null
+//    private var location: Location? = null
+//
+//    private var locationCallback: LocationCallback = object : LocationCallback() {
+//        override fun onLocationResult(locationResult: LocationResult?) {
+//            super.onLocationResult(locationResult)
+//            val locationList = locationResult?.locations
+//
+//            if (locationList?.size!! > 0) {
+//                location = locationList[locationList.size - 1]
+//
+//                currentPosition = LatLng(location!!.latitude, location!!.longitude)
+//
+//                val markerTitle = getCurrentAddress(currentPosition)
+//                val markerSnippet = ("위도:" + location!!.latitude.toString()
+//                        + " 경도:" + location!!.longitude.toString())
+//
+//                setCurrentLocation(location, markerTitle, markerSnippet)
+//                currentLocation = location
+//            }
+//        }
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        return inflater.inflate(R.layout.fragment_maps, container, false)
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//
+//        map_view.run {
+//            onCreate(savedInstanceState)
+//            onResume()
+//            getMapAsync(this@MapsFragment)
+//        }
+//
+//        locationRequest = LocationRequest()
+//            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY) //정확한 위치 요청
+//            .setInterval(UPDATE_INTERVAL_MS.toLong()) //활성 위치 업데이트 간격
+//            .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS.toLong()) //위치 업데이트 간격
+//
+//        LocationSettingsRequest.Builder().addLocationRequest(locationRequest!!)
+//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(App.instance.context())
+//
+//    }
+//
+//    override fun onMapReady(googleMap: GoogleMap?) {
+//        map = googleMap
+//
+//        setDefaultLocation()
+//        startLocationUpdates()
+//
+//        map?.run {
+//            isMyLocationEnabled = true
+//            uiSettings.isMyLocationButtonEnabled = true
+//        }
+//
+//        btn_search.setOnClickListener {
+//            searchLocation()
+//        }
+//    }
+//
+//    private fun startLocationUpdates() {
+//        fusedLocationClient?.requestLocationUpdates(
+//            locationRequest,
+//            locationCallback,
+//            Looper.myLooper()
+//        )
+//    }
+//
+//    fun getCurrentAddress(latLng: LatLng): String {
+//        val geoCoder = Geocoder(App.instance.context(), Locale.getDefault())
+//        val addresses = geoCoder.getFromLocation(
+//            latLng.latitude,
+//            latLng.longitude,
+//            1
+//        )
+//        return addresses[0].getAddressLine(0).toString()
+//    }
+//
+//    fun setCurrentLocation(location: Location?, markerTitle: String, markerSnippet: String) {
+//        currentMarker?.remove()
+//
+//        val currentLatLng = LatLng(location!!.latitude, location.longitude)
+//
+//        val markerOptions = MarkerOptions().apply {
+//            position(currentLatLng)
+//            title(markerTitle)
+//            snippet(markerSnippet)
+//            draggable(true)
+//        }
+//
+//        currentMarker = map?.addMarker(markerOptions)
+//        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+//
+//    }
+//
+//    private fun setDefaultLocation() {
+//        val defaultLocation = LatLng(37.56, 126.97)
+//        val markerTitle = "위치정보 가져올 수 없음"
+//        val markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요"
+//
+//        currentMarker?.remove()
+//
+//        val markerOptions = MarkerOptions().apply {
+//            position(defaultLocation)
+//            title(markerTitle)
+//            snippet(markerSnippet)
+//            draggable(true)
+//            icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+//        }
+//
+//        currentMarker = map?.addMarker(markerOptions)
+//        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 15f))
+//
+//    }
+//
+//    private fun searchLocation() {
+//        map?.clear()
+//        val geoCoder = Geocoder(App.instance.context(), Locale.getDefault())
+//        val addresses = geoCoder.getFromLocationName(
+//            edt_search.text.toString(),
+//            10
+//        )
+//
+//        if(addresses.size > 0){
+//            val searchLatLng = LatLng(addresses[0].latitude, addresses[0].longitude)
+//
+//            val markerTitle = getCurrentAddress(searchLatLng)
+//
+//            val markerOptions = MarkerOptions().apply {
+//                position(searchLatLng)
+//                title(markerTitle)
+//                draggable(true)
+//            }
+//
+//            map?.run {
+//                addMarker(markerOptions)
+//                animateCamera(CameraUpdateFactory.newLatLngZoom(searchLatLng, 15f))
+//            }
+//        }
+//
+//
+//
+//
+//
+//    }
+//
+//    companion object {
+//        private const val UPDATE_INTERVAL_MS = 1000  // 1초
+//        private const val FASTEST_UPDATE_INTERVAL_MS = 500 // 0.5초
+//    }
 }
--- app/src/main/java/com/yeonae/chamelezone/ReviewCreateActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/ReviewCreateActivity.kt
@@ -1,13 +1,64 @@
 package com.yeonae.chamelezone
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_review_create.*
 
 class ReviewCreateActivity : AppCompatActivity() {
+
+    val imgViewList = intArrayOf(
+        R.id.image_view_1,
+        R.id.image_view_2,
+        R.id.image_view_3,
+        R.id.image_view_4
+    )
+
+    private val getGalleryImage = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_create)
 
+        btn_img_1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*"
+            )
+            startActivityForResult(intent, getGalleryImage)
+        }
+
+        btn_img_2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*"
+            )
+            startActivityForResult(intent, getGalleryImage)
+        }
+
+        btn_img_3.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*"
+            )
+            startActivityForResult(intent, getGalleryImage)
+        }
+
+        btn_img_4.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*"
+            )
+            startActivityForResult(intent, getGalleryImage)
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == getGalleryImage && resultCode == RESULT_OK && data != null && data.data != null) {
+            val selectedImageUri = data.data
+            image_view_1.setImageURI(selectedImageUri)
+        }
+    }
 }
