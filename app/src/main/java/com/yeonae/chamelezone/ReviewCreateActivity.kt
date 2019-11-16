7f20a32288e2382ff39b87c783819fe495288e10
LEESUNAE <sjsao5012@gmail.com>
2019-11-16 20:00:45 +0900

    glide implementation

--- app/src/main/java/com/yeonae/chamelezone/ReviewCreateActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/ReviewCreateActivity.kt
@@ -9,21 +9,21 @@ import androidx.appcompat.app.AppCompatActivity
 import com.kroegerama.imgpicker.BottomSheetImagePicker
 import kotlinx.android.synthetic.main.activity_review_create.*
 
-class ReviewCreateActivity : AppCompatActivity()/*, BottomSheetImagePicker.OnImagesSelectedListener */{
-//    override fun onImagesSelected(uris: List<Uri>, tag: String?) {
-////        toast("Result from tag: $tag")
-////
-////        imageContainer.removeAllViews()
-////        uris.forEach { uri ->
-////            val iv = LayoutInflater.from(this).inflate(
-////                R.layout.activity_review_create,
-////                image_view_1,
-////                false
-////            ) as ImageView
-////            imageContainer.addView(iv)
-////            Glide.with(this).load(uri).into(iv)
-////        }
-////    }
+class ReviewCreateActivity : AppCompatActivity(), BottomSheetImagePicker.OnImagesSelectedListener {
+    override fun onImagesSelected(uris: List<Uri>, tag: String?) {
+//        toast("Result from tag: $tag")
+
+        imageContainer.removeAllViews()
+        uris.forEach { uri ->
+            val iv = LayoutInflater.from(this).inflate(
+                R.layout.activity_review_create,
+                image_view_1,
+                false
+            ) as ImageView
+            imageContainer.addView(iv)
+            Glide.with(this).load(uri).into(iv)
+        }
+    }
 
 //    val imgViewList = intArrayOf(
 //        R.id.image_view_1,
