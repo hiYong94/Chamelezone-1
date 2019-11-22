fe36c28d4e7b0fe7c44fe3122ee12564e9a69ca9
LEEYEONJAE <eunoiaa22@gmail.com>
2019-11-22 22:31:15 +0900

    place, review 수정 화면 생성

---
+++ app/src/main/java/com/yeonae/chamelezone/review/ReviewModifyFragment.kt
@@ -0,0 +1,18 @@
+package com.yeonae.chamelezone.review
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yeonae.chamelezone.R
+
+class ReviewModifyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.fragment_review_modify, container, false)
+}
