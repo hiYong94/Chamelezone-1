fb17a669b8be86b9c8434c72ee2f97fe476788d8
LEEYEONJAE <eunoiaa22@gmail.com>
2019-11-22 22:26:22 +0900

    place 화면 생성

---
+++ app/src/main/java/com/yeonae/chamelezone/place/PlaceDetailActivity.kt
@@ -0,0 +1,25 @@
+package com.yeonae.chamelezone.place
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yeonae.chamelezone.adapter.ImageViewPagerAdapter
+import com.yeonae.chamelezone.R
+import com.yeonae.chamelezone.review.ReviewCreateActivity
+import kotlinx.android.synthetic.main.activity_place_detail.*
+
+class PlaceDetailActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_detail)
+
+        val imageAdapter = ImageViewPagerAdapter()
+        view.adapter = imageAdapter
+
+        review.setOnClickListener {
+            val intent = Intent(this, ReviewCreateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
