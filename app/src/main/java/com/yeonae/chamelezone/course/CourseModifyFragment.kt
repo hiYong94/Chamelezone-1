79b7f79ac6cebb87c9351ac49e70388a1e61cf7d
LEEYEONJAE <eunoiaa22@gmail.com>
2019-11-22 22:22:32 +0900

    course 화면 생성

---
+++ app/src/main/java/com/yeonae/chamelezone/course/CourseModifyFragment.kt
@@ -0,0 +1,18 @@
+package com.yeonae.chamelezone.course
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yeonae.chamelezone.R
+
+class CourseModifyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.fragment_course_modify, container, false)
+}
