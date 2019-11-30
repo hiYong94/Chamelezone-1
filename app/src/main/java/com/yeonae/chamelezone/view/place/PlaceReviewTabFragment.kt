841a76be2d1025bbac75050049c4e876bc431976
LEESUNAE <sjsao5012@gmail.com>
2019-11-30 18:40:05 +0900

    장소 상세 페이지 TabLayout, ViewPager

--- app/src/main/java/com/yeonae/chamelezone/view/place/PlaceReviewTabFragment.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/place/PlaceReviewTabFragment.kt
@@ -1,11 +1,14 @@
 package com.yeonae.chamelezone.view.place
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.yeonae.chamelezone.R
+import com.yeonae.chamelezone.view.review.ReviewCreateActivity
+import kotlinx.android.synthetic.main.fragment_place_review_tab.*
 
 class PlaceReviewTabFragment : Fragment() {
 
@@ -15,4 +18,13 @@ class PlaceReviewTabFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? =
         inflater.inflate(R.layout.fragment_place_review_tab, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        review.setOnClickListener {
+            val intent = Intent(context, ReviewCreateActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
