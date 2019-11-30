c494210bc84088c79ba9523601fdbef8d9ca18d3
LEEYEONJAE <eunoiaa22@gmail.com>
2019-11-30 18:53:51 +0900

    course button 수정

--- app/src/main/java/com/yeonae/chamelezone/view/course/CourseRegisterActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/course/CourseRegisterActivity.kt
@@ -3,12 +3,17 @@ package com.yeonae.chamelezone.view.course
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.yeonae.chamelezone.R
+import kotlinx.android.synthetic.main.activity_course_register.*
 
 class CourseRegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_register)
+
+        btn_back.setOnClickListener {
+            finish()
+        }
 
     }
 }
