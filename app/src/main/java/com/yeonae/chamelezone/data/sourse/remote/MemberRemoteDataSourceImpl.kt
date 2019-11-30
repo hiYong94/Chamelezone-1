b4d9976fe79ed0c79de473ac690e3c1a345037e5
LEESUNAE <sjsao5012@gmail.com>
2019-11-30 18:45:28 +0900

    모델 구조 변경, repository/remoteDataSource 생성

---
+++ app/src/main/java/com/yeonae/chamelezone/data/sourse/remote/MemberRemoteDataSourceImpl.kt
@@ -0,0 +1,24 @@
+package com.yeonae.chamelezone.data.sourse.remote
+
+class MemberRemoteDataSourceImpl private constructor(/*api: Api*/): MemberRemoteDataSource {
+    override fun createMember() {
+
+    }
+
+    override fun getMember() {
+
+    }
+
+    override fun deleteMember() {
+
+    }
+
+    override fun updateMember() {
+
+    }
+
+    companion object{
+        fun getInstence() : MemberRemoteDataSource = MemberRemoteDataSourceImpl()
+    }
+
+}
