f9f2892b32e3ce8b8d4fc77513068bc87a5ad5a1
LEEYEONJAE <eunoiaa22@gmail.com>
2019-11-30 14:56:55 +0900

    placeTab 화면 구성

--- app/src/main/java/com/yeonae/chamelezone/view/place/PlaceReviewTabFragment.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/place/PlaceReviewTabFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yeonae.chamelezone.R
+import com.yeonae.chamelezone.model.Like
+import com.yeonae.chamelezone.view.place.adapter.PlaceReviewTabRvAdapter
+import kotlinx.android.synthetic.main.fragment_place_review_tab.*
 
 class PlaceReviewTabFragment : Fragment() {
+    private val placeReviewRvAdapter = PlaceReviewTabRvAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,4 +20,21 @@ class PlaceReviewTabFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? =
         inflater.inflate(R.layout.fragment_place_review_tab, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setAdapter()
+
+        placeReviewRvAdapter.setOnClickListener(object : PlaceReviewTabRvAdapter.OnClickListener {
+            override fun onClick(like: Like) {
+
+            }
+        })
+
+    }
+
+    private fun setAdapter() {
+        recycler_place_review.layoutManager = LinearLayoutManager(context)
+        recycler_place_review.adapter = placeReviewRvAdapter
+    }
 }
