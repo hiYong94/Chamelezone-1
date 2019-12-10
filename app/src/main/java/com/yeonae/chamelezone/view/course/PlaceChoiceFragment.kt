54d2a892211c25184819daf77a8ae2df640b53c9
LEEYEONJAE <eunoiaa22@gmail.com>
2019-12-11 00:20:33 +0900

    코스 등록 - 장소 검색 ui 수정 및 dummy 데이터 생성

--- app/src/main/java/com/yeonae/chamelezone/view/course/PlaceChoiceFragment.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/course/PlaceChoiceFragment.kt
@@ -5,11 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yeonae.chamelezone.R
+import com.yeonae.chamelezone.data.model.Place
+import com.yeonae.chamelezone.view.course.adapter.PlaceChoiceRvAdapter
 import kotlinx.android.synthetic.main.fragment_place_choice.*
 
 class PlaceChoiceFragment : Fragment() {
-
+    private val placeChoiceList = arrayListOf(
+        Place("구슬모아당구장", "전시회, 카페", "서울 용산구 독서당로 85"),
+        Place(
+            "구슬모아당구장", "이연재이연재이연재이연재이연재이연재이연재이연재", "연재네 집은 서울시 강서로18아길 25-6 성재파크빌이다!" +
+                    "천안집은 천안시 동남구 문암로 92-27 신세대원룸이다."
+        )
+    )
+    private val placeChoiceRvAdapter = PlaceChoiceRvAdapter(placeChoiceList)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,6 +29,14 @@ class PlaceChoiceFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        setAdapter()
+
+        placeChoiceRvAdapter.setOnClickListener(object : PlaceChoiceRvAdapter.OnClickListener {
+            override fun onClick(place: Place) {
+
+            }
+        })
 
         view?.setOnClickListener {
             true
@@ -32,4 +50,10 @@ class PlaceChoiceFragment : Fragment() {
             requireActivity().onBackPressed()
         }
     }
+
+    private fun setAdapter() {
+        recycler_place_choice.layoutManager = LinearLayoutManager(context)
+        recycler_place_choice.adapter = placeChoiceRvAdapter
+    }
+
 }
