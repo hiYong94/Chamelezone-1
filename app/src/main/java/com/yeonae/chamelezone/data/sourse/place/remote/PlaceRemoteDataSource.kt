9de8deba9ec3e7aaf029266886d3cf4054ca0047
LEESUNAE <sjsao5012@gmail.com>
2019-12-10 23:20:17 +0900

    PlaceRemoteDataSource 생성

---
+++ app/src/main/java/com/yeonae/chamelezone/data/sourse/place/remote/PlaceRemoteDataSource.kt
@@ -0,0 +1,6 @@
+package com.yeonae.chamelezone.data.sourse.place.remote
+
+interface PlaceRemoteDataSource {
+    fun getSearchByKeyword()
+    fun getLocationPlace()
+}
---
+++ app/src/main/java/com/yeonae/chamelezone/data/sourse/place/remote/PlaceRemoteDataSourceImpl.kt
@@ -0,0 +1,11 @@
+package com.yeonae.chamelezone.data.sourse.place.remote
+
+class PlaceRemoteDataSourceImpl private constructor() : PlaceRemoteDataSource{
+    override fun getSearchByKeyword() {
+
+    }
+
+    override fun getLocationPlace() {
+
+    }
+}
