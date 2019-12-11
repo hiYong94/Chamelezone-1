a6b140329d919d0edf4cf4aeed41f4e5dd1ff4d6
LEEYEONJAE <eunoiaa22@gmail.com>
2019-12-12 01:20:03 +0900

    myplace에 더보기 버튼 클릭시 수정, 삭제 구현

--- app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/adapter/MyPlaceRvAdapter.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/adapter/MyPlaceRvAdapter.kt
@@ -1,11 +1,15 @@
 package com.yeonae.chamelezone.view.mypage.myplace.adapter
 
 import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.yeonae.chamelezone.R
 import com.yeonae.chamelezone.data.model.Place
-import kotlinx.android.synthetic.main.item_like.view.*
+import kotlinx.android.synthetic.main.activity_place_detail.view.*
+import kotlinx.android.synthetic.main.item_my_place.view.*
 
 class MyPlaceRvAdapter(private var items : ArrayList<Place>) : RecyclerView.Adapter<MyPlaceRvAdapter.MyPlaceViewHolder>() {
 
@@ -46,7 +50,17 @@ class MyPlaceRvAdapter(private var items : ArrayList<Place>) : RecyclerView.Adap
                 tv_place_name.text = item.placeName
                 tv_place_keyword.text = item.placeKeyword
                 tv_place_address.text = item.placeAddress
+                btn_more.setOnClickListener {
+                    showPopup(btn_more)
+                }
+
             }
+        }
+        private fun showPopup(v: View) {
+            val popup = PopupMenu(v.context, v)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.pop_up_menu, popup.menu)
+            popup.show()
         }
     }
 }
