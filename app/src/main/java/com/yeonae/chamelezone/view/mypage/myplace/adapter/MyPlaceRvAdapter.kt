e4d2b71fa447b2d1646b5eebd71a8fa5fb2568d1
LEEYEONJAE <eunoiaa22@gmail.com>
2019-12-18 21:05:47 +0900

    나의 장소 - 더보기 버튼 수정

--- app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/adapter/MyPlaceRvAdapter.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/adapter/MyPlaceRvAdapter.kt
@@ -1,17 +1,17 @@
 package com.yeonae.chamelezone.view.mypage.myplace.adapter
 
 import android.view.LayoutInflater
-import android.view.MenuInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.yeonae.chamelezone.R
 import com.yeonae.chamelezone.data.model.Place
-import kotlinx.android.synthetic.main.activity_place_detail.view.*
 import kotlinx.android.synthetic.main.item_my_place.view.*
 
-class MyPlaceRvAdapter(private var items : ArrayList<Place>) : RecyclerView.Adapter<MyPlaceRvAdapter.MyPlaceViewHolder>() {
+class MyPlaceRvAdapter(private var items: ArrayList<Place>) :
+    RecyclerView.Adapter<MyPlaceRvAdapter.MyPlaceViewHolder>() {
 
     //private var items = mutableListOf<Place>()
     private var onClickListener: OnClickListener? = null
@@ -50,12 +50,20 @@ class MyPlaceRvAdapter(private var items : ArrayList<Place>) : RecyclerView.Adap
                 tv_place_name.text = item.placeName
                 tv_place_keyword.text = item.placeKeyword
                 tv_place_address.text = item.placeAddress
-                btn_more.setOnClickListener {
-                    showPopup(btn_more)
-                }
 
+                btn_more.setOnTouchListener(object : View.OnTouchListener {
+                    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                        if (p1?.action == MotionEvent.ACTION_DOWN) {
+                            popup_menu.visibility = View.VISIBLE
+                            popup_menu.x = p1.x
+                            popup_menu.y = p1.y
+                        }
+                        return true
+                    }
+                })
             }
         }
+
         private fun showPopup(v: View) {
             val popup = PopupMenu(v.context, v)
             val inflater = popup.menuInflater
