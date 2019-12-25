3af10997b15489ab8d3748374a39e77d6b39edd3
LEESUNAE <sjsao5012@gmail.com>
2019-12-25 22:42:19 +0900

    장소상세페이지 스크롤 시 뒤로가기 버튼 예외처리

--- app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.yeonae.chamelezone.view.place
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.yeonae.chamelezone.R
 import com.yeonae.chamelezone.adapter.ImageViewPagerAdapter
@@ -17,6 +19,10 @@ class PlaceDetailActivity : AppCompatActivity() {
             finish()
         }
 
+        btn_back_2.setOnClickListener {
+            finish()
+        }
+
         val imageAdapter = ImageViewPagerAdapter()
         view.adapter = imageAdapter
         tab_layout.setupWithViewPager(view, true)
@@ -24,6 +30,21 @@ class PlaceDetailActivity : AppCompatActivity() {
         val fragmentAdapter = PlaceDetailPagerAdapter(supportFragmentManager)
         viewpager_detail.adapter = fragmentAdapter
         tabs_detail.setupWithViewPager(viewpager_detail)
+
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        Log.d("ssssssssssssssssssss", "${scroll_view.scrollY}")
+        Log.d("imageView", "${view.bottom}")
+
+        if (scroll_view.scrollY <= img_layout.bottom) {
+            layout_visibility.visibility = View.GONE // 화면에서 제외
+            btn_back.visibility = View.VISIBLE
+        } else {
+            layout_visibility.visibility = View.VISIBLE // 화면에서 보이기
+            btn_back.visibility = View.GONE
+        }
+    }
 }
