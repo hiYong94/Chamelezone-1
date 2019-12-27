a6d071c1bd810fae63d94e5f392af18cf2dfeea3
LEEYEONJAE <eunoiaa22@gmail.com>
2019-12-27 16:45:31 +0900

    retrofit 수정 및 MemberApi, PlaceApi 생성

--- app/src/main/java/com/yeonae/chamelezone/network/api/RetrofitConnection.kt
+++ app/src/main/java/com/yeonae/chamelezone/network/api/RetrofitConnection.kt
@@ -3,11 +3,12 @@ package com.yeonae.chamelezone.network.api
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitConnection {
-    private var retrofit = Retrofit.Builder()
+object RetrofitConnection {
+    private val retrofit = Retrofit.Builder()
         .baseUrl("http://13.209.136.122:3000")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    var service = retrofit.create<RetrofitInterface>(RetrofitInterface::class.java)
+    val memberService = retrofit.create<MemberApi>(MemberApi::class.java)
+    val placeService = retrofit.create<PlaceApi>(PlaceApi::class.java)
 }
