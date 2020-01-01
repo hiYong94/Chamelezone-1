d92aa1f4b4183aedd5c64d220d5c3804e65111c5
LEESUNAE <sjsao5012@gmail.com>
2020-01-01 23:12:49 +0900

    홈 탭 화면에 장소 이미지 추가

--- app/src/main/java/com/yeonae/chamelezone/view/home/adapter/HomePlaceRvAdapter.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/home/adapter/HomePlaceRvAdapter.kt
@@ -5,9 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.yeonae.chamelezone.view.place.PlaceDetailActivity
 import com.yeonae.chamelezone.R
 import com.yeonae.chamelezone.data.model.Place
+import com.yeonae.chamelezone.view.place.PlaceDetailActivity
 import kotlinx.android.synthetic.main.item_place_list.view.*
 
 class HomePlaceRvAdapter(var placeList: ArrayList<Place>) :
