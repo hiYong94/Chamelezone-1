cb7ae73cd0948e06da0028e5a1c199e204d6a661
LEEYEONJAE <eunoiaa22@gmail.com>
2020-01-03 02:15:27 +0900

    member remote source 생성자 받는 걸로 수정

--- app/src/main/java/com/yeonae/chamelezone/data/source/remote/member/MemberRemoteDataSourceImpl.kt
+++ app/src/main/java/com/yeonae/chamelezone/data/source/remote/member/MemberRemoteDataSourceImpl.kt
@@ -1,58 +1,44 @@
 package com.yeonae.chamelezone.data.source.remote.member
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import com.google.gson.JsonObject
-import com.yeonae.chamelezone.App
-import com.yeonae.chamelezone.network.api.MemberApi
+import com.yeonae.chamelezone.data.repository.member.MemberCallBack
 import com.yeonae.chamelezone.network.api.RetrofitConnection
-import com.yeonae.chamelezone.network.model.MemberResponse
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MemberRemoteDataSourceImpl private constructor() :
+class MemberRemoteDataSourceImpl private constructor(private var retrofitConnection: RetrofitConnection) :
     MemberRemoteDataSource {
-    private val retrofitConnection = RetrofitConnection
-    private val context: Context = App.instance.context()
     override fun createMember(
         email: String,
-        passwrd: String,
+        password: String,
         name: String,
-        nickname: String,
-        phone: String
+        nickName: String,
+        phone: String,
+        callBack: MemberCallBack
     ) {
-        val memberResponse = MemberResponse(
-            1,
-            email,
-            passwrd,
-            name,
-            nickname,
-            phone,
-            ""
-        )
         val jsonObject = JsonObject().apply {
-            addProperty("email", memberResponse.email)
-            addProperty("password", memberResponse.password)
-            addProperty("name", memberResponse.name)
-            addProperty("nickName", memberResponse.nickName)
-            addProperty("phoneNumber", memberResponse.phoneNumber)
+            addProperty("email", email)
+            addProperty("password", password)
+            addProperty("name", name)
+            addProperty("nickName", nickName)
+            addProperty("phoneNumber", phone)
         }
 
         retrofitConnection.memberService.userRegister(jsonObject).enqueue(object :
-            Callback<MemberResponse> {
+            Callback<ResponseBody> {
             override fun onResponse(
-                call: Call<MemberResponse>,
-                response: Response<MemberResponse>
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
             ) {
-                if (response.isSuccessful) {
-                    Toast.makeText(context, "회원가입 성공", Toast.LENGTH_LONG)
-                        .show()
-                }
+                callBack.onSuccess("회원가입 성공")
+                Log.d("tag", response.body().toString())
             }
 
-            override fun onFailure(call: Call<MemberResponse>, t: Throwable) {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                 Log.e("tag", t.toString())
             }
         })
@@ -72,6 +58,7 @@ class MemberRemoteDataSourceImpl private constructor() :
     }
 
     companion object {
-        fun getInstance(): MemberRemoteDataSource = MemberRemoteDataSourceImpl()
+        fun getInstance(retrofitConnection: RetrofitConnection): MemberRemoteDataSource =
+            MemberRemoteDataSourceImpl(retrofitConnection)
     }
 }
