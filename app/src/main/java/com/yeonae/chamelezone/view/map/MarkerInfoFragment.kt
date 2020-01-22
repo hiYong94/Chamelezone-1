e2db9cef57e7b6e3cc7934e52c699dad136adf74
LEEYEONJAE <eunoiaa22@gmail.com>
2020-01-23 01:08:03 +0900

    지도에서 장소 검색 성공, 사용하지 않는 함수 제거

--- app/src/main/java/com/yeonae/chamelezone/view/map/MarkerInfoFragment.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/map/MarkerInfoFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.yeonae.chamelezone.R
+import com.yeonae.chamelezone.network.model.PlaceResponse
+import kotlinx.android.synthetic.main.fragment_marker_info.*
 
 class MarkerInfoFragment : Fragment() {
 
@@ -18,6 +20,22 @@ class MarkerInfoFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        tv_place_name.text = arguments!!.getString("placeName")
+        tv_place_keyword.text = arguments!!.getString("placeKeyword")
+        tv_place_address.text = arguments!!.getString("placeAddress")
 
+    }
+
+    companion object {
+        fun newInstance(
+            placeInfo: PlaceResponse
+        ) = MarkerInfoFragment().apply {
+            arguments = Bundle().apply {
+                putString("placeName", placeInfo.name)
+                putString("placeKeyword", placeInfo.keywordName)
+                putString("placeAddress", placeInfo.address)
+            }
+
+        }
     }
 }
