326c46437f989cd5eecc16661fb61a0733119d3c
LEESUNAE <sjsao5012@gmail.com>
2020-01-23 00:21:08 +0900

    review RemoteDataSource 생성

--- app/src/main/java/com/yeonae/chamelezone/data/source/remote/review/ReviewRemoteDataSource.kt
+++ app/src/main/java/com/yeonae/chamelezone/data/source/remote/review/ReviewRemoteDataSource.kt
@@ -1,4 +1,17 @@
 package com.yeonae.chamelezone.data.source.remote.review
 
+import com.yeonae.chamelezone.data.repository.review.ReviewCallBack
+
 interface ReviewRemoteDataSource {
+    fun createReview(
+        placeName: String,
+        nickname: String,
+        reviewImg: String,
+        content: String,
+        callBack: ReviewCallBack<String>
+    )
+
+    fun getReview()
+    fun updateReview()
+    fun deleteReview()
 }
