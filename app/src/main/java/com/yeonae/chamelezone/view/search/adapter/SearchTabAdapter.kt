af173dde1304cc35a5930c4658a9214887ee689b
LEEYEONJAE <eunoiaa22@gmail.com>
2020-01-23 00:57:02 +0900

    class명 수정

--- app/src/main/java/com/yeonae/chamelezone/view/search/adapter/SearchTabAdapter.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/search/adapter/SearchTabAdapter.kt
@@ -3,20 +3,20 @@ package com.yeonae.chamelezone.view.search.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.yeonae.chamelezone.view.search.AreaNameTabFragment
+import com.yeonae.chamelezone.view.search.AddressTabFragment
 import com.yeonae.chamelezone.view.search.KeywordTabFragment
 import com.yeonae.chamelezone.view.search.PlaceNameTabFragment
 
 class SearchTabAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     private val placeNameTabFragment = PlaceNameTabFragment()
-    private val areaNameTabFragment = AreaNameTabFragment()
+    private val addressTabFragment = AddressTabFragment()
     private val keywordTabFragment = KeywordTabFragment()
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
             0 -> placeNameTabFragment
-            1 -> areaNameTabFragment
+            1 -> addressTabFragment
             else -> keywordTabFragment
         }
     }
