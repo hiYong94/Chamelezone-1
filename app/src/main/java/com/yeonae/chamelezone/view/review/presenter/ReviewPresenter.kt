e31438a91a4da7460ce9e92482e0b0833491972c
LEESUNAE <sjsao5012@gmail.com>
2020-01-23 00:20:15 +0900

    review presenter 생성

---
+++ app/src/main/java/com/yeonae/chamelezone/view/review/presenter/ReviewPresenter.kt
@@ -0,0 +1,12 @@
+package com.yeonae.chamelezone.view.review.presenter
+
+class ReviewPresenter(private val reviewView: ReviewContract.View): ReviewContract.Presenter{
+    override fun reviewCreate(
+        placeName: String,
+        nickname: String,
+        reviewImg: String,
+        content: String
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
