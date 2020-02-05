4a8de004d1f4b9f74a5af3d5b0a14b0f70faf3ee
LEESUNAE <sjsao5012@gmail.com>
2020-02-06 03:11:31 +0900

    reviewApi image multipart 및 getReviewList 추가

--- app/src/main/java/com/yeonae/chamelezone/network/api/ReviewApi.kt
+++ app/src/main/java/com/yeonae/chamelezone/network/api/ReviewApi.kt
@@ -1,14 +1,22 @@
 package com.yeonae.chamelezone.network.api
 
 import com.google.gson.JsonObject
+import com.yeonae.chamelezone.network.model.ReviewResponse
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ReviewApi {
+    @Multipart
     @POST("/review")
     fun reviewCreate(
-        @Body review: JsonObject
+        @Body review: JsonObject,
+        @Part("file\" filename=\"pp.png\" ") file: RequestBody
     ): Call<ResponseBody>
+
+    @GET("/review")
+    fun getReviewList(
+        @Path("reviewNumber") reviewNumber: Int
+    ): Call<ReviewResponse>
 }
