ac3ea5f37619ecd29a1c5342bed9bcb317bd15da
LEESUNAE <sjsao5012@gmail.com>
2020-02-10 22:11:01 +0900

    장소 상세 화면 image size modify

--- app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.yeonae.chamelezone.view.place
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.yeonae.chamelezone.R
 import com.yeonae.chamelezone.adapter.ImageViewPagerAdapter
@@ -8,11 +9,26 @@ import com.yeonae.chamelezone.view.place.adapter.PlaceDetailPagerAdapter
 import kotlinx.android.synthetic.main.activity_place_detail.*
 
 class PlaceDetailActivity : AppCompatActivity() {
-    private val PLACE_NAME = "placeName"
-    private val PLACE_NUMBER = "placeNumber"
+
+
+    companion object{
+        private const val PLACE_NAME = "placeName"
+        private const val PLACE_NUMBER = "placeNumber"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_detail)
+
+        vp.post {
+            vp.layoutParams = vp.layoutParams.apply {
+                height = ((vp.parent as ViewGroup).width / 3) * 2
+            }
+            setupView()
+        }
+
+    }
+
+    private fun setupView(){
         val placeName = intent.getStringExtra(PLACE_NAME)
         val placeNumber = intent.getIntExtra(PLACE_NUMBER, 0)
         tv_place_name.text = placeName
@@ -22,8 +38,8 @@ class PlaceDetailActivity : AppCompatActivity() {
         }
 
         val imageAdapter = ImageViewPagerAdapter()
-        view.adapter = imageAdapter
-        tab_layout.setupWithViewPager(view, true)
+        vp.adapter = imageAdapter
+        tab_layout.setupWithViewPager(vp, true)
 
         val fragmentAdapter = PlaceDetailPagerAdapter(supportFragmentManager, placeNumber)
         viewpager_detail.adapter = fragmentAdapter
