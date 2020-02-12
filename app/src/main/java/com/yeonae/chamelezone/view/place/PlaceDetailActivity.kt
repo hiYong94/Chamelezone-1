bc3684b492489f833ce270f27fb5de4332ade497
LEEYEONJAE <eunoiaa22@gmail.com>
2020-02-13 01:13:19 +0900

    장소 상세 이미지 가져오기

--- app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
@@ -1,15 +1,33 @@
 package com.yeonae.chamelezone.view.place
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.yeonae.chamelezone.Injection
 import com.yeonae.chamelezone.R
 import com.yeonae.chamelezone.adapter.ImageViewPagerAdapter
+import com.yeonae.chamelezone.network.model.PlaceResponse
 import com.yeonae.chamelezone.view.place.adapter.PlaceDetailPagerAdapter
+import com.yeonae.chamelezone.view.place.presenter.PlaceInfoContract
+import com.yeonae.chamelezone.view.place.presenter.PlaceInfoPresenter
 import kotlinx.android.synthetic.main.activity_place_detail.*
 
-class PlaceDetailActivity : AppCompatActivity() {
+class PlaceDetailActivity : AppCompatActivity(), PlaceInfoContract.View {
+    override fun placeInfo(place: PlaceResponse) {
+        val placeImages = place.savedImageName.split(",")
+        val images = arrayListOf<String>()
+        Log.d("placeImages", placeImages.toString())
+        for(i in placeImages.indices){
+            images.add("http://13.209.136.122:3000/image/"+ placeImages[i])
+        }
+        val imageAdapter = ImageViewPagerAdapter(images)
+        view.adapter = imageAdapter
+    }
+
+    override lateinit var presenter: PlaceInfoContract.Presenter
     private val PLACE_NAME = "placeName"
     private val PLACE_NUMBER = "placeNumber"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_detail)
@@ -17,12 +35,16 @@ class PlaceDetailActivity : AppCompatActivity() {
         val placeNumber = intent.getIntExtra(PLACE_NUMBER, 0)
         tv_place_name.text = placeName
 
+        presenter = PlaceInfoPresenter(
+            Injection.placeRepository(), this
+        )
+
+        presenter.placeDetail(placeNumber)
+
         btn_back.setOnClickListener {
             finish()
         }
 
-        val imageAdapter = ImageViewPagerAdapter()
-        view.adapter = imageAdapter
         tab_layout.setupWithViewPager(view, true)
 
         val fragmentAdapter = PlaceDetailPagerAdapter(supportFragmentManager, placeNumber)
