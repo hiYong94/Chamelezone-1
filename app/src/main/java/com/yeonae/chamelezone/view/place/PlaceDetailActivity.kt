475f0fdba7638c2d9ed70404b8ad1ab341c08e60
LEESUNAE <sjsao5012@gmail.com>
2020-02-13 04:21:10 +0900

    장소 상세 화면 스크롤 시 수정

--- app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/place/PlaceDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.yeonae.chamelezone.view.place
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.yeonae.chamelezone.R
@@ -23,6 +25,7 @@ class PlaceDetailActivity : AppCompatActivity() {
             vp.layoutParams = vp.layoutParams.apply {
                 height = ((vp.parent as ViewGroup).width / 3) * 2
             }
+
             setupView()
         }
 
@@ -44,5 +47,30 @@ class PlaceDetailActivity : AppCompatActivity() {
         val fragmentAdapter = PlaceDetailPagerAdapter(supportFragmentManager, placeNumber)
         viewpager_detail.adapter = fragmentAdapter
         tabs_detail.setupWithViewPager(viewpager_detail)
+
+        tool_bar.post {
+            tool_bar.apply {
+                val nameBar = layout_visibility.height
+                val tabBar = tabs_detail.height
+
+                Log.d("nameBar", nameBar.toString())
+                Log.d("tabBar", tabBar.toString())
+                layoutParams = tool_bar.layoutParams.apply {
+                    height = nameBar + tabBar
+                }
+                Log.d("toolBarHeight", height.toString())
+                Log.d("layoutBottom", layout.bottom.toString())
+
+                scroll.viewTreeObserver.addOnScrollChangedListener {
+                    if (scroll.scrollY <= layout.bottom) {
+                        layout_visibility.visibility = View.GONE // 화면에서 제외
+                        btn_back.visibility = View.VISIBLE
+                    } else {
+                        layout_visibility.visibility = View.VISIBLE // 화면에서 보이기
+                        btn_back.visibility = View.GONE
+                    }
+                }
+            }
+        }
     }
 }
