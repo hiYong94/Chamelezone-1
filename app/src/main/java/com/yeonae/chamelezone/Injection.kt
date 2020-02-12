0e22a2799ad92f5329cc632ec6dcbea5abecc46b
LEEYEONJAE <eunoiaa22@gmail.com>
2020-02-13 01:02:13 +0900

    injection placeRepository 수정 및 courseRepository 함수 추가

--- app/src/main/java/com/yeonae/chamelezone/Injection.kt
+++ app/src/main/java/com/yeonae/chamelezone/Injection.kt
@@ -1,11 +1,14 @@
 package com.yeonae.chamelezone
 
 import android.content.Context
+import com.yeonae.chamelezone.data.repository.course.CourseRepository
+import com.yeonae.chamelezone.data.repository.course.CourseRepositoryImpl
 import com.yeonae.chamelezone.data.repository.member.MemberRepository
 import com.yeonae.chamelezone.data.repository.member.MemberRepositoryImpl
 import com.yeonae.chamelezone.data.repository.place.PlaceRepository
 import com.yeonae.chamelezone.data.repository.place.PlaceRepositoryImpl
 import com.yeonae.chamelezone.data.source.local.member.MemberLocalDataSourceImpl
+import com.yeonae.chamelezone.data.source.remote.course.CourseRemoteDataSourceImpl
 import com.yeonae.chamelezone.data.source.remote.member.MemberRemoteDataSourceImpl
 import com.yeonae.chamelezone.data.source.remote.place.PlaceRemoteDataSourceImpl
 import com.yeonae.chamelezone.network.api.RetrofitConnection
@@ -19,9 +22,18 @@ object Injection {
             MemberLocalDataSourceImpl.getInstance(AppExecutors(), UserDatabase.getInstance(context))
         )
     }
-    fun placeRepository(context: Context) : PlaceRepository {
+
+    fun placeRepository(): PlaceRepository {
         return PlaceRepositoryImpl.getInstance(
             PlaceRemoteDataSourceImpl.getInstance(RetrofitConnection.placeService)
+        )
+    }
+
+    fun courseRepository(): CourseRepository {
+        return CourseRepositoryImpl.getInstance(
+            CourseRemoteDataSourceImpl.getInstance(
+                RetrofitConnection.courseService
+            )
         )
     }
 }
