ad85b506486be6c0c8f842bd2f4e838276c46ad5
LEEYEONJAE <eunoiaa22@gmail.com>
2020-02-13 01:52:13 +0900

    course detail contract, presenter 구현, 적용

---
+++ app/src/main/java/com/yeonae/chamelezone/view/course/presenter/CourseDetailContract.kt
@@ -0,0 +1,15 @@
+package com.yeonae.chamelezone.view.course.presenter
+
+import com.yeonae.chamelezone.network.model.CourseResponse
+
+interface CourseDetailContract {
+
+    interface View {
+        var presenter: Presenter
+        fun showCourseDetail(course: CourseResponse)
+    }
+
+    interface Presenter {
+        fun getCourseDetail(courseNumber: Int)
+    }
+}
