16dc1c44198cc460db69e04f52d468252752d3a1
LEEYEONJAE <eunoiaa22@gmail.com>
2020-02-13 01:24:11 +0900

    좋아요 로그인 화면, 로그아웃 화면 구현

--- app/src/main/java/com/yeonae/chamelezone/view/like/LikeTabFragment.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/like/LikeTabFragment.kt
@@ -7,13 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.yeonae.chamelezone.Injection
 import com.yeonae.chamelezone.R
 import com.yeonae.chamelezone.data.model.Place
 import com.yeonae.chamelezone.view.like.adapter.LikeTabRvAdapter
+import com.yeonae.chamelezone.view.like.presenter.LikeContract
+import com.yeonae.chamelezone.view.like.presenter.LikePresenter
+import com.yeonae.chamelezone.view.login.LoginActivity
 import com.yeonae.chamelezone.view.place.PlaceDetailActivity
 import kotlinx.android.synthetic.main.fragment_like_tab.*
 
-class LikeTabFragment : Fragment() {
+class LikeTabFragment : Fragment(), LikeContract.View {
+    override lateinit var presenter: LikeContract.Presenter
     private val likeList = arrayListOf(
         Place("구슬모아당구장", "전시회, 카페", "서울 용산구 독서당로 85", "", "place1"),
         Place("론리드프로젝트", "빨래방, 카페", "서울 용산구 신흥로 78", "", "place2"),
@@ -21,24 +26,50 @@ class LikeTabFragment : Fragment() {
     )
     private val likeTabRvAdapter = LikeTabRvAdapter(likeList)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+    override fun showResultView(response: Boolean) {
+        if (response) {
+            layout_before_login.visibility = View.GONE
+            layout_after_login.visibility = View.VISIBLE
+            setAdapter()
+            likeTabRvAdapter.setOnClickListener(object : LikeTabRvAdapter.OnClickListener {
+                override fun onClick(place: Place) {
+                    val intent = Intent(context, PlaceDetailActivity::class.java)
+                    startActivity(intent)
+                }
+            })
+        } else {
+            layout_before_login.visibility = View.VISIBLE
+            layout_after_login.visibility = View.GONE
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
         inflater.inflate(R.layout.fragment_like_tab, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        setAdapter()
-
-        likeTabRvAdapter.setOnClickListener(object : LikeTabRvAdapter.OnClickListener {
-            override fun onClick(place: Place) {
-                val intent = Intent(context, PlaceDetailActivity::class.java)
-                startActivity(intent)
-            }
-        })
+        presenter = LikePresenter(
+            Injection.memberRepository(requireContext()), this
+        )
+        btn_login.setOnClickListener {
+            val intent = Intent(context, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun setAdapter() {
         recycler_like.layoutManager = LinearLayoutManager(context)
         recycler_like.adapter = likeTabRvAdapter
     }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.checkLogin()
+    }
+
 }
 
