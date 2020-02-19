57d018a2902975a8ea48d2b3d2d290ea1593a2ad
LEEYEONJAE <eunoiaa22@gmail.com>
2020-02-19 23:19:17 +0900

    장소 상세 - 좋아요 기능 추가 및 LikeNumber, MemberNumber 변수 추가

--- app/src/main/java/com/yeonae/chamelezone/view/place/presenter/PlaceDetailContract.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/place/presenter/PlaceDetailContract.kt
@@ -3,16 +3,21 @@ package com.yeonae.chamelezone.view.place.presenter
 import com.yeonae.chamelezone.network.model.PlaceResponse
 import com.yeonae.chamelezone.network.room.entity.UserEntity
 
-interface PlaceInfoContract {
+interface PlaceDetailContract {
 
     interface View {
         var presenter: Presenter
         fun placeInfo(place: PlaceResponse)
+        fun showResultView(response: Boolean)
         fun showUserInfo(user: UserEntity)
+        fun showLikeState(response: String)
     }
 
     interface Presenter {
         fun placeDetail(placeNumber: Int, memberNumber: Int)
+        fun checkLogin()
         fun getUser()
+        fun selectLike(memberNumber: Int, placeNumber: Int)
+        fun deleteLike(likeNumber: Int, memberNumber: Int, placeNumber: Int)
     }
 }
