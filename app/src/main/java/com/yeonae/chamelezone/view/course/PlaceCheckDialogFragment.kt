c21c715f379221d7de60d23482cb3a59606dcf1d
LEEYEONJAE <eunoiaa22@gmail.com>
2020-03-05 02:11:35 +0900

    코스 등록 - 장소 검색 diglog fragment로 수정 및 중복 장소 넣을 수 없게 수정

---
+++ app/src/main/java/com/yeonae/chamelezone/view/course/PlaceCheckDialogFragment.kt
@@ -0,0 +1,142 @@
+package com.yeonae.chamelezone.view.course
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.yeonae.chamelezone.Injection
+import com.yeonae.chamelezone.R
+import com.yeonae.chamelezone.data.model.PlaceItem
+import com.yeonae.chamelezone.ext.shortToast
+import com.yeonae.chamelezone.view.course.adapter.PlaceCheckRvAdapter
+import com.yeonae.chamelezone.view.search.presenter.SearchContract
+import com.yeonae.chamelezone.view.search.presenter.SearchPresenter
+import kotlinx.android.synthetic.main.fragment_place_check_dialog.*
+import kotlinx.android.synthetic.main.item_place_check.*
+
+class PlaceCheckDialogFragment : DialogFragment(), SearchContract.View {
+    override lateinit var presenter: SearchContract.Presenter
+    private val placeChoiceRvAdapter = PlaceCheckRvAdapter()
+    private var heightRatio = 0.8803125f  // default
+    private var widthRatio = 0.88888889f // default
+    private var onClickListener: OnClickListener? = null
+    private lateinit var lastCheckedPlace: PlaceItem
+    var checkStatus: Boolean = false
+
+    interface OnClickListener {
+        fun onClick(place: PlaceItem, placeIndex: Int)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        onClickListener = (context as OnClickListener)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val placeIndex = arguments?.getInt("placeIndex")
+        val dpMetrics = DisplayMetrics()
+        activity?.windowManager?.defaultDisplay?.getMetrics(dpMetrics)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        arguments?.getFloat(DIALOG_HEIGHT_RATIO)?.let {
+            if (it > 0) {
+                heightRatio = it
+            }
+        }
+        arguments?.getFloat(DIALOG_WIDTH_RATIO)?.let {
+            if (it > 0) {
+                widthRatio = it
+            }
+        }
+        dialog?.window?.setLayout(
+            (dpMetrics.widthPixels * widthRatio).toInt(),
+            (dpMetrics.heightPixels * heightRatio).toInt()
+        )
+
+        presenter = SearchPresenter(
+            Injection.placeRepository(), this
+        )
+        setAdapter()
+
+        placeChoiceRvAdapter.setOnClickListener(object : PlaceCheckRvAdapter.OnClickListener {
+            override fun onClick(place: PlaceItem) {
+                Log.d("placeCheck", btn_check.isChecked.toString())
+                if (btn_check.isChecked) {
+                    lastCheckedPlace = place
+                    checkStatus = true
+                } else if (!btn_check.isChecked) {
+                    checkStatus = false
+                }
+            }
+        })
+
+        btn_search.setOnClickListener {
+            presenter.searchByName("${edt_search.text}")
+        }
+
+        edt_search.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE || i == EditorInfo.IME_ACTION_NEXT || i == EditorInfo.IME_ACTION_SEARCH || i == EditorInfo.IME_ACTION_GO) {
+                presenter.searchByName("${edt_search.text}")
+            }
+            true
+        }
+
+        btn_ok.setOnClickListener {
+            if (checkStatus && placeIndex != null) {
+                onClickListener?.onClick(lastCheckedPlace, placeIndex)
+                dialog?.cancel()
+            } else if (!checkStatus) {
+                context?.shortToast("장소를 선택해주세요.")
+            }
+        }
+
+        btn_cancel.setOnClickListener {
+            dialog?.cancel()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_place_check_dialog, container, false)
+    }
+
+    override fun showPlaceList(placeList: List<PlaceItem>) {
+        layout_no_search.visibility = View.GONE
+        layout_has_search.visibility = View.VISIBLE
+        placeChoiceRvAdapter.addData(placeList)
+    }
+
+    override fun showMessage(message: String) {
+        layout_no_search.visibility = View.VISIBLE
+        layout_has_search.visibility = View.GONE
+        tv_message.text = message
+    }
+
+    private fun setAdapter() {
+        recycler_place_choice.layoutManager = LinearLayoutManager(context)
+        recycler_place_choice.adapter = placeChoiceRvAdapter
+    }
+
+    companion object {
+        const val DIALOG_HEIGHT_RATIO = "DIALOG_HEIGHT_RATIO"
+        const val DIALOG_WIDTH_RATIO = "DIALOG_WIDTH_RATIO"
+        fun newInstance(placeIndex: Int): PlaceCheckDialogFragment {
+            val frag = PlaceCheckDialogFragment()
+            val args = Bundle()
+            args.putInt("placeIndex", placeIndex)
+            frag.arguments = args
+            return frag
+        }
+    }
+}
---
+++ app/src/main/java/com/yeonae/chamelezone/view/course/adapter/PlaceCheckRvAdapter.kt
@@ -0,0 +1,70 @@
+package com.yeonae.chamelezone.view.course.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yeonae.chamelezone.R
+import com.yeonae.chamelezone.data.model.PlaceItem
+import com.yeonae.chamelezone.ext.glideImageSet
+import kotlinx.android.synthetic.main.item_place_check.view.*
+
+class PlaceCheckRvAdapter :
+    RecyclerView.Adapter<PlaceCheckRvAdapter.PlaceChoiceViewHolder>() {
+    private var selectedPosition = -1
+    private val items = mutableListOf<PlaceItem>()
+    private var onClickListener: OnClickListener? = null
+
+    interface OnClickListener {
+        fun onClick(place: PlaceItem)
+    }
+
+    fun setOnClickListener(listener: OnClickListener) {
+        onClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceChoiceViewHolder =
+        PlaceChoiceViewHolder(parent)
+
+    override fun getItemCount(): Int =
+        items.size
+
+    override fun onBindViewHolder(holder: PlaceChoiceViewHolder, position: Int) {
+        holder.bind(position, items[position], onClickListener)
+    }
+
+    fun addData(addDataList: List<PlaceItem>) {
+        items.clear()
+        items.addAll(addDataList)
+        notifyDataSetChanged()
+    }
+
+    inner class PlaceChoiceViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_place_check, parent, false)
+    ) {
+        fun bind(position: Int, item: PlaceItem, listener: OnClickListener?) {
+            itemView.run {
+                btn_check.isChecked = selectedPosition == position
+                Log.d("btn_check", selectedPosition.toString())
+                Log.d("btn_check", position.toString())
+                btn_check.setOnClickListener {
+                    if (selectedPosition == position) {
+                        btn_check.isChecked = false
+                        selectedPosition = -1
+                    } else {
+                        listener?.onClick(item)
+                        selectedPosition = position
+                        notifyDataSetChanged()
+                    }
+                }
+                tv_place_name.text = item.name
+                tv_place_keyword.text = item.keyword
+                tv_place_address.text = item.address
+                iv_place_image.glideImageSet(
+                    item.image, iv_place_image.measuredWidth,
+                    iv_place_image.measuredHeight
+                )
+            }
+        }
+    }
+}
