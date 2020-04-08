bca4587c595c8fcd71f77a0f317e900c0a200321
LEEYEONJAE <eunoiaa22@gmail.com>
2020-04-09 00:54:25 +0900

    장소 수정

--- app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/presenter/PlaceModifyContract.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/presenter/PlaceModifyContract.kt
@@ -1,7 +1,9 @@
 package com.yeonae.chamelezone.view.mypage.myplace.presenter
 
+import com.yeonae.chamelezone.data.repository.place.PlaceCallBack
 import com.yeonae.chamelezone.network.model.KeywordResponse
 import com.yeonae.chamelezone.network.model.PlaceResponse
+import java.math.BigDecimal
 
 interface PlaceModifyContract {
 
@@ -9,10 +11,22 @@ interface PlaceModifyContract {
         var presenter: Presenter
         fun showPlaceDetail(place: PlaceResponse)
         fun showKeywordList(response: List<KeywordResponse>)
+        fun showResult(response: Boolean)
     }
 
     interface Presenter {
         fun getPlaceDetail(placeNumber: Int, memberNumber: Int?)
         fun getKeyword()
+        fun updatePlace(
+            placeNumber: Int,
+            images: List<String>,
+            memberNumber: Int,
+            address: String,
+            phoneNumber: String,
+            content: String,
+            latitude: BigDecimal,
+            longitude: BigDecimal,
+            imageNumber: List<Int>
+        )
     }
 }
--- app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/presenter/PlaceModifyPresenter.kt
+++ app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/presenter/PlaceModifyPresenter.kt
@@ -4,6 +4,7 @@ import com.yeonae.chamelezone.data.repository.place.PlaceCallBack
 import com.yeonae.chamelezone.data.repository.place.PlaceRepository
 import com.yeonae.chamelezone.network.model.KeywordResponse
 import com.yeonae.chamelezone.network.model.PlaceResponse
+import java.math.BigDecimal
 
 class PlaceModifyPresenter(
     private val repository: PlaceRepository,
@@ -34,4 +35,27 @@ class PlaceModifyPresenter(
 
         })
     }
+    override fun updatePlace(
+        placeNumber: Int,
+        images: List<String>,
+        memberNumber: Int,
+        address: String,
+        phoneNumber: String,
+        content: String,
+        latitude: BigDecimal,
+        longitude: BigDecimal,
+        imageNumber: List<Int>
+    ) {
+        repository.updatePlace(placeNumber, images, memberNumber, address, phoneNumber, content, latitude, longitude, imageNumber, object : PlaceCallBack<Boolean>{
+            override fun onSuccess(response: Boolean) {
+                view.showResult(response)
+            }
+
+            override fun onFailure(message: String) {
+
+            }
+
+        })
+    }
+
 }
