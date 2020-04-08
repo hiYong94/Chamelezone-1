d65a08735cc3e75afcad91fbd55e3acfe9116d10
LEEYEONJAE <eunoiaa22@gmail.com>
2020-04-09 00:53:24 +0900

    영업시간 수정

---
+++ app/src/main/java/com/yeonae/chamelezone/view/mypage/myplace/presenter/OpeningHoursPresenter.kt
@@ -0,0 +1,22 @@
+package com.yeonae.chamelezone.view.mypage.myplace.presenter
+
+import com.yeonae.chamelezone.data.repository.place.PlaceCallBack
+import com.yeonae.chamelezone.data.repository.place.PlaceRepository
+
+class OpeningHoursPresenter(
+    private val repository: PlaceRepository,
+    private val view: OpeningHoursContract.View
+) : OpeningHoursContract.Presenter {
+    override fun updateOpeningHours(placeNumber: Int, openingTimes: List<String>) {
+        repository.updateOpeningHours(placeNumber, openingTimes, object : PlaceCallBack<Boolean> {
+            override fun onSuccess(response: Boolean) {
+                view.showResult(response)
+            }
+
+            override fun onFailure(message: String) {
+
+            }
+
+        })
+    }
+}
